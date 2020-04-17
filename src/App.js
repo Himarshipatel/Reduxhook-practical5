b46b41b4164c49d2302d8c244a5460b1f3426c62
Himarshipatel <62707959+Himarshipatel@users.noreply.github.com>
2020-04-17 15:18:25 +0530

    Chat add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Bar } from "react-chartjs-2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const state = {
+  labels: ["USA", "CHINA", "AUSTRALIA", "INDIA", "CANADA"],
+  datasets: [
+    {
+      label: "Covid-19",
+      backgroundColor: "rgba(75,192,192,1)",
+      borderColor: "rgba(0,0,0,1)",
+      borderWidth: 2,
+      data: [81, 100, 80, 71, 56],
+    },
+  ],
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Bar
+          data={state}
+          options={{
+            title: {
+              display: true,
+              text: "country Cases",
+              fontSize: 20,
+            },
+            legend: {
+              display: true,
+              position: "right",
+            },
+          }}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
